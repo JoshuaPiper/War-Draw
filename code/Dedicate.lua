801ebb464737459c330395b2424736d085ca2d0e
Joshua Sun <sqh1078534232@gmail.com>
2017-09-10 11:00:21 -0400

    Create Dedicate.lua

---
+++ code/Dedicate.lua
@@ -0,0 +1,240 @@
+--# Dedicate
+function dedicate()
+    alpha.tintd=50
+    tint(255,150)
+    sprite("Documents:Halo",1/2*WIDTH,1/2*HEIGHT,0.9*HEIGHT)
+    tint(128+127*player[1].enlight)
+    player[1]:draw(1/2*WIDTH,9/10*HEIGHT-30,350)
+    tint(128+127*player[2].enlight)
+    player[2]:draw(1/5*WIDTH,3/5*HEIGHT,350)
+    tint(128+127*player[3].enlight)
+    player[3]:draw(1/3*WIDTH,1/4*HEIGHT,350)
+    tint(128+127*player[4].enlight)
+    player[4]:draw(2/3*WIDTH,1/4*HEIGHT,350)
+    tint(128+127*player[5].enlight)
+    player[5]:draw(4/5*WIDTH,3/5*HEIGHT,350)
+    tint(128+127*pressable)
+    sprite("Documents:Choice_Yes",1/2*WIDTH,1/2*HEIGHT+30,250)
+    tint(255)
+    sprite("Documents:Choice_No",1/2*WIDTH,1/2*HEIGHT-30,250)
+    if CurrentTouch.x>=1/2*WIDTH-175 
+        and CurrentTouch.x<=1/2*WIDTH+175 
+        and CurrentTouch.y>=9/10*HEIGHT-233-30
+        and CurrentTouch.y<=9/10*HEIGHT+50-30
+        and CurrentTouch.state==ENDED then
+        for q=1,5 do
+            player[q].enlight=0
+        end
+        player[1].enlight=1
+        if temp==1 or temp==10 then
+            s=math.abs(1-turn)
+            if math.abs(1-turn)>=math.floor(1/2*playernum+0.5) then
+                s=playernum-s
+            end
+            if player[1].weapon[3]==2 then
+                s = s + 1
+            end
+            if player[turn].weapon[3]==1 then
+                s = s - 1
+            end
+            if temp==1 then
+                if s<=player[turn].range then
+                    pressable=1
+                else
+                    pressable=0
+                end
+            end
+            if temp==10 then
+                if s<=1 then
+                    pressable=1
+                else
+                    pressable=0
+                end
+            end
+        else
+            pressable=1
+        end
+    end
+    if CurrentTouch.x>=1/3*WIDTH-175 
+        and CurrentTouch.x<=1/3*WIDTH+175 
+        and CurrentTouch.y>=1/4*HEIGHT-233 
+        and CurrentTouch.y<=1/4*HEIGHT+50 
+        and CurrentTouch.state==ENDED then
+        for q=1,5 do
+            player[q].enlight=0
+        end
+        player[3].enlight=1
+        if temp==1 or temp==10 then
+            s=math.abs(3-turn)
+            if math.abs(3-turn)>=math.floor(1/2*playernum+0.5) then
+                s=playernum-s
+            end
+            if player[3].weapon[3]==2 then
+                s = s + 1
+            end
+            if player[turn].weapon[3]==1 then
+                s = s - 1
+            end
+            if temp==1 then
+                if s<=player[turn].range then
+                    pressable=1
+                else
+                    pressable=0
+                end
+            end
+            if temp==10 then
+                if s<=1 then
+                    pressable=1
+                else
+                    pressable=0
+                end
+            end
+        else
+            pressable=1
+        end
+    end
+    if CurrentTouch.x>=2/3*WIDTH-175 
+        and CurrentTouch.x<=2/3*WIDTH+175 
+        and CurrentTouch.y>=1/4*HEIGHT-233 
+        and CurrentTouch.y<=1/4*HEIGHT+50 
+        and CurrentTouch.state==ENDED then
+        for q=1,5 do
+            player[q].enlight=0
+        end
+        player[4].enlight=1
+        if temp==1 or temp==10 then
+            s=math.abs(4-turn)
+            if math.abs(4-turn)>=math.floor(1/2*playernum+0.5) then
+                s=playernum-s
+            end
+            if player[4].weapon[3]==2 then
+                s = s + 1
+            end
+            if player[turn].weapon[3]==1 then
+                s = s - 1
+            end
+            if temp==1 then
+                if s<=player[turn].range then
+                    pressable=1
+                else
+                    pressable=0
+                end
+            end
+            if temp==10 then
+                if s<=1 then
+                    pressable=1
+                else
+                    pressable=0
+                end
+            end
+        else
+            pressable=1
+        end
+    end
+    if CurrentTouch.x>=1/5*WIDTH-175 
+        and CurrentTouch.x<=1/5*WIDTH+175 
+        and CurrentTouch.y>=3/5*HEIGHT-233 
+        and CurrentTouch.y<=3/5*HEIGHT+50 
+        and CurrentTouch.state==ENDED then
+        for q=1,5 do
+            player[q].enlight=0
+        end
+        player[2].enlight=1
+        if temp==1 or temp==10 then
+            s=math.abs(2-turn)
+            if math.abs(2-turn)>=math.floor(1/2*playernum+0.5) then
+                s=playernum-s
+            end
+            if player[2].weapon[3]==2 then
+                s = s + 1
+            end
+            if player[turn].weapon[3]==1 then
+                s = s - 1
+            end
+            if temp==1 then
+                if s<=player[turn].range then
+                    pressable=1
+                else
+                    pressable=0
+                end
+            end
+            if temp==10 then
+                if s<=1 then
+                    pressable=1
+                else
+                    pressable=0
+                end
+            end
+        else
+            pressable=1
+        end
+    end
+    if CurrentTouch.x>=4/5*WIDTH-175 
+        and CurrentTouch.x<=4/5*WIDTH+175 
+        and CurrentTouch.y>=3/5*HEIGHT-233 
+        and CurrentTouch.y<=3/5*HEIGHT+50 
+        and CurrentTouch.state==ENDED then
+        for q=1,5 do
+            player[q].enlight=0
+        end
+        player[5].enlight=1
+        if temp==1 or temp==10 then
+            s=math.abs(5-turn)
+            if math.abs(5-turn)>=math.floor(1/2*playernum+0.5) then
+                s=playernum-s
+            end
+            if player[5].weapon[3]==2 then
+                s = s + 1
+            end
+            if player[turn].weapon[3]==1 then
+                s = s - 1
+            end
+            if temp==1 then
+                if s<=player[turn].range then
+                    pressable=1
+                else
+                    pressable=0
+                end
+            end
+            if temp==10 then
+                if s<=1 then
+                    pressable=1
+                else
+                    pressable=0
+                end
+            end
+        else
+            pressable=1
+        end
+    end
+    for q=1,5 do
+        if player[q].enlight==1 and q==turn and (temp==1 or temp==19 or temp==8 or temp==9 or temp==10) then
+            pressable=0
+        end
+    end
+    if CurrentTouch.x>=1/2*WIDTH-125
+        and CurrentTouch.x<=1/2*WIDTH+125
+        and CurrentTouch.y>=1/2*HEIGHT-20
+        and CurrentTouch.y<=1/2*HEIGHT+80
+        and CurrentTouch.state==ENDED 
+        and pressable==1 then
+        for q=1,5 do
+            if player[q].enlight==1 then
+                pressed3=0
+            end
+        end
+                
+    end
+    if CurrentTouch.x>=1/2*WIDTH-125
+        and CurrentTouch.x<=1/2*WIDTH+125
+        and CurrentTouch.y>=1/2*HEIGHT-80
+        and CurrentTouch.y<=1/2*HEIGHT+20
+        and CurrentTouch.state==ENDED 
+        and pressable==1 then
+        for q=1,5 do
+            player[q].enlight=0
+            pressed3=0
+        end
+                
+    end
+end
